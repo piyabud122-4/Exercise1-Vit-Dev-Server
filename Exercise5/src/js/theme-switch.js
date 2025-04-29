bb902e460b27b83ccc412161fa1075a19e8c5a4c
Piyabud <piyabud.d@kkumail.com>
2025-04-29 22:39:52 +0700

    Add Exercise5

--- Exercise5/src/js/theme-switch.js
+++ Exercise5/src/js/theme-switch.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     let dark = localStorage.getItem('dark-mode')
     const themeSwitch = document.getElementById('theme-switch')
 
-    const enableDarkmode = () => {
+    const enableDarkMode = () => {
         document.body.classList.add('dark-mode')
         localStorage.setItem('dark-mode', 'active')
     }
 
-    const disableDarkmode = () => {
+    const disableDarkMode = () => {
         document.body.classList.remove('dark-mode')
         localStorage.setItem('dark-mode', null)
     }
 
-    if(dark === "active") enableDarkmode()
+    if(dark === "active") enableDarkMode()
 
     themeSwitch.addEventListener("click", () => {
         dark = localStorage.getItem('dark-mode')
-        dark !== "active" ? enableDarkmode() : disableDarkmode()
+        dark !== "active" ? enableDarkMode() : disableDarkMode()
     })
 })
